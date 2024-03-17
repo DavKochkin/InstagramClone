ebd1eb08fd825ee7f898224563dbf542e6b0df5a
David Kochkin <kochkin.david@gmail.com>
2024-03-17 16:33:51 +0200

    Storage Operations

--- InstagramClone/UploadViewController.swift
+++ InstagramClone/UploadViewController.swift
@@ -20,15 +20,15 @@ class UploadViewController: UIViewController, UIImagePickerControllerDelegate, U
         super.viewDidLoad()
 
         imageView.isUserInteractionEnabled = true
-        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(chooseImage))
+        let gestureRecognizer              = UITapGestureRecognizer(target: self, action: #selector(chooseImage))
         imageView.addGestureRecognizer(gestureRecognizer )
     }
     
     
     @objc func chooseImage() {
         
-        let pickerController = UIImagePickerController()
-        pickerController.delegate = self
+        let pickerController        = UIImagePickerController()
+        pickerController.delegate   = self
         pickerController.sourceType = .photoLibrary
         present(pickerController, animated: true)
     }
@@ -37,6 +37,14 @@ class UploadViewController: UIViewController, UIImagePickerControllerDelegate, U
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         imageView.image = info[.originalImage] as? UIImage
         self.dismiss(animated: true)
+    }
+    
+    func makeAlert(titleInput: String, messageInput: String) {
+        let alert    = UIAlertController(title: titleInput, message: messageInput, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default)
+        alert.addAction(okButton)
+        self.present(alert, animated: true)
+        
     }
     
     @IBAction func actionButtonClicked(_ sender: Any) {
@@ -48,10 +56,12 @@ class UploadViewController: UIViewController, UIImagePickerControllerDelegate, U
         
         if let data = imageView.image?.jpegData(compressionQuality: 0.5) {
             
-            let imageReference = mediaFolder.child("image.jpg")
+            let uuid = UUID().uuidString
+            
+            let imageReference = mediaFolder.child("\(uuid).jpg")
             imageReference.putData(data, metadata: nil) { (metadata, error) in
                 if error != nil {
-                    print(error?.localizedDescription)
+                    self.makeAlert(titleInput: "Error", messageInput: error?.localizedDescription ?? "Error")
                 } else {
                     
                     imageReference.downloadURL { (url, error) in
@@ -59,7 +69,10 @@ class UploadViewController: UIViewController, UIImagePickerControllerDelegate, U
                         if error == nil {
                             
                             let imageUrl = url?.absoluteString
-                            print(imageUrl)
+                            
+                            
+                            // Database
+                            
                         }
                     }
                 }
