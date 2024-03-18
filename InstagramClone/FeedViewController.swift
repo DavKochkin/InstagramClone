aef79414246e75354c5ad63e928bfb685add0f3a
David Kochkin <kochkin.david@gmail.com>
2024-03-18 15:15:53 +0200

    Add FeedCell

--- InstagramClone/FeedViewController.swift
+++ InstagramClone/FeedViewController.swift
@@ -7,15 +7,32 @@
 
 import UIKit
 
-class FeedViewController: UIViewController {
+class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
-    
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+    
+        tableView.delegate   = self
+        tableView.dataSource = self
     }
     
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! FeedCell
+        cell.userEmailLabel.text = "David Ko"
+        cell.likeLabel.text      = "10"
+        cell.commentLabel.text   = "Here is your comment"
+        cell.userImageView.image = UIImage(named: "select.png")
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
 
 }
